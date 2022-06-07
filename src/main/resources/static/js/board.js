992ef0958a62ab02b265b10c964d9d3a661f0897
김주원 <sa6812000@naver.com>
2022-06-07 18:10:42 +0900

    글 수정, 삭제

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,7 +1,16 @@
 let index={
 	init: function(){
-		$("#btn-save").on("click", ()=>{ // function(){}, ()=>{} this를 바인딩하기 위해서
+		$("#btn-save").on("click", ()=>{ // function(){}, ()=>{} this를 바인딩하기
+											// 위해서
 			this.save();
+		});
+		$("#btn-delete").on("click", ()=>{ // function(){}, ()=>{} this를 바인딩하기
+											// 위해서
+			this.deleteById();
+		});
+		$("#btn-update").on("click", ()=>{ // function(){}, ()=>{} this를 바인딩하기
+											// 위해서
+			this.update();
 		});
 	},
 
@@ -23,7 +32,43 @@ let index={
 		}).fail(function(error){
 			alert(JSON.stringify(error));
 		});
-	}
+	},
+	
+	deleteById: function(){
+		let id = $("#id").text();
+		$.ajax({
+			type: "DELETE",
+			url: "/api/board/" + id,
+			dataType: "json",
+		}).done(function(resp){
+			alert("삭제가 완료되었습니다.");
+			location.href="/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+	},
+	
+	update: function(){
+		let id = $("#id").val();
+		
+		let data={
+				title: $("#title").val(),
+				content: $("#content").val(),
+			}
+			
+			$.ajax({
+				type: "PUT",
+				url: "/api/board/"+id,
+				data: JSON.stringify(data),
+				contentType: "application/json;charset=utf-8",
+				dataType: "json",
+			}).done(function(resp){
+				alert("글수정이 완료되었습니다.");
+				location.href="/";
+			}).fail(function(error){
+				alert(JSON.stringify(error));
+			});
+		},
 }
 
 index.init();
